07fd59127f694f4fb2f50dbbcb626919708f0e44
Yu Cao <ycao@redhat.com>
2020-05-13 13:30:38 -0400

    fix typo

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -49,7 +49,7 @@ func init() {
 }
 
 var _ = BeforeSuite(func() {
-	By("Setup Hub client")
+	By("Setup client")
 	gvrPolicy = schema.GroupVersionResource{Group: "policies.open-cluster-management.io", Version: "v1", Resource: "policies"}
 	gvrTrustedContainerPolicy = schema.GroupVersionResource{Group: "policies.ibm.com", Version: "v1alpha1", Resource: "trustedcontainerpolicies"}
 	gvrEvent = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "events"}
